86c34ffbda15be95f8690b2b7b5c2b3487e16174
Henrique <henrique05022007@gmail.com>
2026-03-03 19:58:34 -0300

    feat: exercicio 3 03/03/2026

---
+++ modulo06_03-03/exercicio3.js
@@ -0,0 +1,17 @@
+import leia from "readline-sync";
+
+let nome;
+let idade;
+let primeiraDoacao;
+
+nome = leia.question("Digite o nome do doador: ");
+idade = leia.questionInt("Digite a idade do doador: ");
+primeiraDoacao = leia.keyIn("Primeira doacao de sangue? (y/f) ");
+
+if (idade >= 18 && idade <= 59){
+    console.log(`${nome} está apto para doar sangue!`);
+}else if (idade >= 60 && idade <= 69 && primeiraDoacao == "false"){
+    console.log(`${nome} está apto para doar sangue!`);
+}else{
+    console.log(`${nome} não está apto para doar sangue!`);
+}
