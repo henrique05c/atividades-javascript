432e1f95123c79f785ec66ae847e655e97729697
Henrique <henrique05022007@gmail.com>
2026-03-05 22:49:37 -0300

    feat: atividade typescript modulo 08 lista 1 exercicio 2 05/03/2026

---
+++ modulo08_ts_05-03/lista1_ex02.ts
@@ -0,0 +1,17 @@
+import readlinesync from "readline-sync";
+
+const numeros: Array<number> = [2,5,1,3,4,9,7,8,10,6];
+let numeroDigitado: number;
+let encontrado: boolean = false;
+
+numeroDigitado = readlinesync.questionInt("Digite o numero que voce deseja encontrar: ");
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] === numeroDigitado){
+        console.log(`O numero ${numeroDigitado} esta localizado na posicao: ${i}`);
+        encontrado = true;
+        break
+    }
+} if(encontrado === false){
+    console.log(`O numero ${numeroDigitado} nao foi encontrado`)
+}
